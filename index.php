cd1ef8021f233dc2fceea84b941f901bdd578f3a
iwoz <ilonawozniak29@gmail.com>
2021-06-15 14:00:19 +0200

    aktualizacja

--- index.php
+++ index.php
@@ -13,20 +13,25 @@
 <head>
     <meta charset="utf-8">
     <title>Osadnicy</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+
 </head>
 
 <body>
+    <div id="container" >
 
-<p><h2>Lepszej gry żeś jeszcze nie widział. Hyżo podonżaj za mno!</h2> </p>
-<br>
-   <a href="rejestracja.php">Rejestracja </a>
-</br> </br>
+    <p><h2>Lepszej gry żeś jeszcze nie widział. Hyżo podonżaj za mno!</h2> </p> 
+    <br>
+    <a href="rejestracja.php">Rejestracja </a>
+    </br> </br>
 
-<form action="zaloguj.php" method="post">
-Login: </br><input type="text" name="login"></br>
-Hasło: </br><input type="password" name="haslo"></br>
-<input type="submit" value="Zaloguj się"> 
-</form>
+    <div class="form">
+    <form action="zaloguj.php" method="post">
+    Login: </br><input type="text" name="login"></br>
+    Hasło: </br><input type="password" name="haslo"></br>
+    <input type="submit" value="Zaloguj się"> 
+    </form>
+    </div>
  
 
     <?php
@@ -38,7 +43,7 @@ Hasło: </br><input type="password" name="haslo"></br>
 
     ?>
 
-
+    </div>
 </body>
 
 </html>
--- rejestracja.php
+++ rejestracja.php
@@ -1,202 +1,264 @@
-
 <?php
-    session_start();
-    //sprawdzenie czy taka zmienna istnieje, jeśli tak oznacza to, że formularz rejestracji został już wysłany
-    if(isset($_POST['nick'])){
-        //udana walidacja
-        $wszystko_ok = true;
-        //sprawdzenie poprawności nick
-        $nick = $_POST['nick'];
-        //sprawdzenie czy w zostało cokolwiek wpisane w formularz
-        //sprawdzenie długości łańcucha
-        if((strlen($nick)<3) || (strlen($nick)>20))
-        {
-            $wszystko_ok = false;
-            $_SESSION['e_nick'] = "Nick musi posiadać od 3 do 20 znaków.";
-        }
- 
-        if(ctype_alnum($nick) == false)
-        {
-            $wszystko_ok = false;
-            $_SESSION['e_nick'] = "Nick może składać się z tylko liter i cyfr.";
-        }
 
-        //sprawdzenie poprawności adresu email 
-        $email = $_POST['email'];
-        $email_ok = filter_var($email, FILTER_SANITIZE_EMAIL);
+	session_start();
+	  //sprawdzenie czy taka zmienna istnieje, jeśli tak oznacza to, że formularz rejestracji został już wysłany
+	if (isset($_POST['nick']))
+	{
+		//Udana walidacja
+		$wszystko_OK=true;
+		
+		//Sprawdź poprawność nick
+		$nick = $_POST['nick'];
+		//sprawdzenie czy w zostało cokolwiek wpisane w formularz
+		//Sprawdzenie długości łańcucha
+		if ((strlen($nick)<3) || (strlen($nick)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Nick musi posiadać od 3 do 20 znaków!";
+		}
+		
+		if (ctype_alnum($nick)==false)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Nick może składać się tylko z liter i cyfr (bez polskich znaków)";
+		}
+		
+		// Sprawdź poprawność adresu email
+		$email = $_POST['email'];
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_email']="Podaj poprawny adres e-mail!";
+		}
+		
+		//Sprawdź poprawność hasła
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
+		}
+		
+		if ($haslo1!=$haslo2)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
+		}	
 
-        if((filter_var($email_ok, FILTER_VALIDATE_EMAIL)==false) || ($email_ok!=$email))
-        {
-            $wszystko_ok = false;
-            $_SESSION['e_email'] = "Podaj poprawny adres e-mail.";
-        }
+		$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+		
+		//czy zaakceptowano regulamin
+		if (!isset($_POST['regulamin']))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
+		}				
+		
+		//sprawdzenie czy użytkownik to robot
+		$sekret = "6LeRUCkbAAAAAAlpVmSJTUXQSRuVUwl7LUGb9AnH";
+		
+		$sprawdz = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$sekret.'&response='.$_POST['g-recaptcha-response']);
+		
+		$odpowiedz = json_decode($sprawdz);
+		
+		if ($odpowiedz->success==false)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_bot']="Potwierdź, że nie jesteś botem";
+		}		
+		
+		//Zapamiętaj wprowadzone dane
+		$_SESSION['fr_nick'] = $nick;
+		$_SESSION['fr_email'] = $email;
+		$_SESSION['fr_haslo1'] = $haslo1;
+		$_SESSION['fr_haslo2'] = $haslo2;
+		if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
+		
+		require_once "connect.php";
+		//ustawienie sposobu raportowania błędów, które od teraz ma być oparte o wyjątki a nie o ostrzeżenia 
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		
+		try 
+		{
+			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+			if ($polaczenie->connect_errno!=0)
+			{
+				throw new Exception(mysqli_connect_errno());
+			}
+			else
+			{
+				//sprawdzenie czy email już istnieje
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE email='$email'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_maili = $rezultat->num_rows;
+				if($ile_takich_maili>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
+				}		
 
-        //sprawdzenie poprawności hasła
-        $haslo1 = $_POST['haslo1'];
-        $haslo2 = $_POST['haslo2'];
-
-        if((strlen($haslo1)<8) || (strlen($haslo1)>20))
-        {
-            $wszystko_ok = false;
-            $_SESSION['e_haslo'] = "Hasło musi posiadać od 8 do 20 znaków.";
-        }
-
-        if($haslo1 != $haslo2)
-        {
-            $wszystko_ok = false;
-            $_SESSION['e_haslo'] = "Podane hasła nie są identyczne.";
-        }
-
-        $haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
-
-        //czy zakceptowano regulamin
-        if(!isset($_POST['regulamin']))
-         {
-            $wszystko_ok = false;
-            $_SESSION['e_regulamin'] = "Nie zaakceptowano regulaminu.";
-         }
-
-         //sprawdzenie czy uzytkownik to robot
-         $sekret = "6LcEhgUbAAAAAJYzycrM5MTQvAim9uNsmeHotE4c";
-         $sprawdz = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$sekret.'&response='.$_POST['g-recaptcha-response']);
-
-         $odpowiedz= json_decode($sprawdz);
-         if($odpowiedz->success==false)
-         {
-            $wszystko_ok = false;
-            $_SESSION['e_bot'] = "Potwierdź że nie jesteś botem.";
-         }
-
-         require_once('connect.php');
-
-         //ustawienie sposobu raportowania błędów, które od teraz ma być oparte o wyjątki a nie o ostrzeżenia 
-         mysqli_report(MYSQLI_REPORT_STRICT);
-         try
-          {
-            $polaczenie = new mysqli($host,$db_user,$db_password, $db_name);
-
-            if($polaczenie->connect_errno != 0)
-            {
-                throw new Exception(mysqli_connect_errno());
-            }
-            else //połączenie sie udało
-            {
-                //sprawdzenie czy email już istnieje
-                $rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE email='$email'");
-
-                if(!$rezultat) throw new Exception($polaczenie->error);
-
-                $ile_maili = $rezultat->num_rows;
-                if($ile_maili > 0)
-                {
-                    $wszystko_ok = false;
-                    $_SESSION['e_email'] = "Istnieje już konto przypisane do tego adresu email";
-                }
-                     //sprawdzenie czy nick już istnieje
-                $rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE user='$nick'");
-
-                if(!$rezultat) throw new Exception($polaczenie->error);
-
-                $ile_nickow = $rezultat->num_rows;
-                if($ile_nickow > 0)
-                {
-                    $wszystko_ok = false;
-                    $_SESSION['e_nick'] = "Istnieje już gracz o takim nicku. Wybierz inny.";
-                }
-
-                if($wszystko_ok == true)
-                {
-                    //Testy zaliczone
-                   
-                    exit();
-                }
-
-                $polaczenie->close();
-            }
-         }
-            catch(Exception $e)
-         {
-             echo '<span style="color:red">Błąd serwera. Posimy o rejestracje w innym terminie</span>';
-             echo '</br> Informacja developerska' . $e;
-         }
-
-  
-    }
+				//sprawdzenie czy nick już istnieje
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE user='$nick'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_nickow = $rezultat->num_rows;
+				if($ile_takich_nickow>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_nick']="Istnieje już gracz o takim nicku! Wybierz inny.";
+				}
+				
+				if ($wszystko_OK==true)
+				{
+					//Testy zaliczone
+					
+					if ($polaczenie->query("INSERT INTO uzytkownicy VALUES (NULL, '$nick', '$haslo_hash', '$email', 100, 100, 100, 14)"))
+					{
+						$_SESSION['udanarejestracja']=true;
+						header('Location: witamy.php');
+					}
+					else
+					{
+						throw new Exception($polaczenie->error);
+					}
+					
+				}
+				
+				$polaczenie->close();
+			}
+			
+		}
+		catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+			echo '<br />Informacja developerska: '.$e;
+		}
+		
+	}
+	
+	
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="pl">
-    <header>
-        <meta charset="utf-8">
-        <title>Rejsetracja</title>
-        <script src="https://www.google.com/recaptcha/api.js"></script>
-        <style>
-            .error
-            {
-                color:red;
-                margin-top: 10px;
-                margin-bottom: 10px;
-            }
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Osadnicy - załóż darmowe konto!</title>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<link rel="stylesheet" href="style.css"  type="text/css" >
+	
+	<style>
+		.error
+		{
+			color:red;
+			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+	</style>
+</head>
 
-        </style>
-    <header>
-
-    <body>
-
-        <h2>Zarejestruj się za darmo już dziś!</h2>
-        <form method="post">
-            Nick: </br> <input name="nick" type="text"> </br>
-            <?php
-                if(isset($_SESSION['e_nick']))
-                {
-                    echo'<div class="error">' . $_SESSION['e_nick'] . "</div>";
-                    //nalezy wyczyścić tą zmienną sesyjną, inaczej błąd będzie się pojawiał nawet przy prawidłowym woisaniu nicku
-                    unset($_SESSION['e_nick']);
-                }
-            ?>
-            E-mail: </br> <input name="email" type="text"> </br>
-
-                <?php
-                    if(isset($_SESSION['e_email']))
-                    {
-                        echo '<div class="error" >' . $_SESSION['e_email'] . '</div>';
-                        unset($_SESSION['e_email']);
-                    }
-                ?>
-            Hasło: </br> <input name="haslo1" type="password"> </br>
-
-            <?php
-                if(isset($_SESSION['e_haslo']))
-                {
-                    echo '<div class = "error">' . $_SESSION['e_haslo'] . '</div>';
-                    unset($_SESSION['e_haslo']);
-                }
-            ?>
-            Powtórz Hasło: </br> <input name="haslo2" type="password"> </br>
-            <label><input type="checkbox" name="regulamin"> Akceptuję regulamin</label> </br>
-            <?php
-                if(isset($_SESSION['e_regulamin']))
-                {
-                    echo '<div class = "error">' . $_SESSION['e_regulamin'] . '</div>';
-                    unset($_SESSION['e_regulamin']);
-                }
-            ?>
-            <!-- dodanie pola o nie byciu robotem-->
-            <div class="g-recaptcha" data-sitekey="6LcEhgUbAAAAAKh8JRQ5Pp1PNsniQcYcM6SFIIFw"></div>
-            <?php
-                if(isset($_SESSION['e_bot']))
-                {
-                    echo '<div class = "error">' . $_SESSION['e_bot'] . '</div>';
-                    unset($_SESSION['e_bot']);
-                }
-            ?>
-            </br>
-            <input type="submit" value="Zarejestruj się">
-         </form>
-    </body>
-
- 
-
-
-
+<body>
+	<div id="container">
+		<form method="post" class="form">
+			<h2>Zarejestruj się</h2>
+			Nickname: <br /> <input type="text" value="<?php
+				if (isset($_SESSION['fr_nick']))
+				{
+					echo $_SESSION['fr_nick'];
+					unset($_SESSION['fr_nick']);
+				}
+			?>" name="nick" /><br />
+			
+			<?php
+				if (isset($_SESSION['e_nick']))
+				{
+					echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+					 //nalezy wyczyścić tą zmienną sesyjną, inaczej błąd będzie się pojawiał nawet przy prawidłowym woisaniu nicku
+					unset($_SESSION['e_nick']);
+				}
+			?>
+			
+			E-mail: <br /> <input type="text" value="<?php
+				if (isset($_SESSION['fr_email']))
+				{
+					echo $_SESSION['fr_email'];
+					unset($_SESSION['fr_email']);
+				}
+			?>" name="email" /><br />
+			
+			<?php
+				if (isset($_SESSION['e_email']))
+				{
+					echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+					unset($_SESSION['e_email']);
+				}
+			?>
+			
+			Twoje hasło: <br /> <input type="password"  value="<?php
+				if (isset($_SESSION['fr_haslo1']))
+				{
+					echo $_SESSION['fr_haslo1'];
+					unset($_SESSION['fr_haslo1']);
+				}
+			?>" name="haslo1" /><br />
+			
+			<?php
+				if (isset($_SESSION['e_haslo']))
+				{
+					echo '<div class="error">'.$_SESSION['e_haslo'].'</div>';
+					unset($_SESSION['e_haslo']);
+				}
+			?>		
+			
+			Powtórz hasło: <br /> <input type="password" value="<?php
+				if (isset($_SESSION['fr_haslo2']))
+				{
+					echo $_SESSION['fr_haslo2'];
+					unset($_SESSION['fr_haslo2']);
+				}
+			?>" name="haslo2" /><br />
+			
+			<label>
+				<input type="checkbox" name="regulamin" <?php
+				if (isset($_SESSION['fr_regulamin']))
+				{
+					echo "checked";
+					unset($_SESSION['fr_regulamin']);
+				}
+					?>/> Akceptuję regulamin
+			</label>
+			
+			<?php
+				if (isset($_SESSION['e_regulamin']))
+				{
+					echo '<div class="error">'.$_SESSION['e_regulamin'].'</div>';
+					unset($_SESSION['e_regulamin']);
+				}
+			?>	
+			<!-- dodanie pola o nie byciu robotem-->
+			<div class="g-recaptcha" data-sitekey="6LeRUCkbAAAAAK4pyJt9lvJ2CVlda1qwGdyRMUTq"></div>
+			
+			<?php
+				if (isset($_SESSION['e_bot']))
+				{
+					echo '<div class="error">'.$_SESSION['e_bot'].'</div>';
+					unset($_SESSION['e_bot']);
+				}
+			?>	
+			
+			<br />
+			
+			<input type="submit" value="Zarejestruj się" />
+			
+		</form>
+		</div>
+</body>
 </html>
